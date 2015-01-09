723be14591e1d3a951f2285a2a2e3834a80660d2
Omar Lopez <pollo.es.pollo@gmail.com>
2015-01-09 22:34:37 +0100

    Bugs olvidado el end de la función !!

--- plugins/apq.lua
+++ plugins/apq.lua
@@ -1,11 +1,12 @@
 function run(msg, matches)
-	
+
 local receiver = get_receiver(msg)
 local text = msg.text:sub(6,-1)
 file = assert(io.open("/home/azureuser/projects/a.txt", "a+"))
 file:write(text,"\n")
 file:close()
-
+return 'Añadida quote'
+end
 
 return {
     description = "Version",
