d07d5b8dc30a91e8bbca8f72b88bdf2254f4d407
Omar Lopez <pollo.es.pollo@gmail.com>
2015-01-09 21:25:34 +0100

    Get quotes from text file

--- plugins/papoi.lua
+++ plugins/papoi.lua
@@ -1,30 +1,13 @@
 function run(msg, matches)
-   t = {"Papoi perro lastimero", 
-"Mira los aliens tuvieron la culpa", 
-"En cuarto milenio dicen que ...",
-"Pues vamonos de vacaciones a Iran es un pais normal",
-"Ereala Aurrera",
-"El pais mas seguro es Iran",
-"Acaba de pasar Ben Johnson por delante del Etxebe", 
-"Acaba de pasar Tristan Ulloa por delante del Etxebe", 
-"Acaba de pasar El Duo Sacapuntas por delante del Etxebe", 
-"Acaba de pasar el Crasty por delante del Etxebe", 
-"Acaba de pasar Carmen Martez Bordiu por delante del Etxebe", 
-"Acaba de pasar mi padre por delante del Etxebe", 
-"Acaba de pasar un tipo con boina por delante del Etxebe", 
-"Acaba de pasar Antonio Resines por delante del Etxebe", 
-"Acaba de pasar Juanma Iturriaga por delante del Etxebe", 
-"Acaba de pasar Fernando Romay por delante del Etxebe", 
-"Acaba de pasar el primo del Gran Wyoming por delante del Etxebe", 
-"Acaba de pasar Nikki Lauda por delante del Etxebe", 
-"Soy como el hermano feo de los Calatrava",
-"A mi no se me cae el pelo. Ni se me caer",
-"En la calle Penyagolosa me invitaron a un chino",
-"Me ha dicho un cliente que Torrente 6 se rodar en Navarra",
-"Cualquier cosa vasca, mola. Hasta el culo de un camello vasco",
-"Acaban de venir dos putas lituanas del piso de enfrente a pedirme cambio de 50"
-}
-   return (t[math.random(23)])
+i=0
+quotes= {}
+file = assert(io.open("/home/azureuser/projects/quotes_papoi.txt", "r"))
+for line in file:lines() do 
+ quotes[i] = line
+ i=i+1
+end
+file:close()
+   return (quotes[math.random(#quotes)])
 end
 
 return {
@@ -33,4 +16,3 @@ return {
     patterns = {"^!papoi$"},
     run = run
 }
-
